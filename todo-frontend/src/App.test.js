eac351c91d1f1eede3a13cd33475c2a79e939122
Abdullah Süha ışık <abdullahsuhaisik@gmail.com>
2021-10-07 00:36:29 +0300

    Ttd approach started

--- todo-frontend/src/App.test.js
+++ todo-frontend/src/App.test.js
@@ -1,8 +1,37 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Check hole TODO', () => {
+  test('renders todo header', () => {
+    render(<App />);
+    const headerElemen = screen.getByText('TODOS');
+    expect(headerElemen).toBeInTheDocument();
+    // Todo Header
+    // Input exist
+    // Button Exist
+    // Typing Input and Press button remove input
+  });
+  test('renders todo input', () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText('add todo')
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test('renders add todo button', () => {
+    render(<App />);
+    const addTodoItemElement = screen.getByRole('button')
+    expect(addTodoItemElement).toBeInTheDocument();
+  });
+
+  test('press todo button', async () => {
+    render(<App />);
+    const addTodoItemElement = screen.getByRole('button')
+    expect(addTodoItemElement).toBeInTheDocument();
+    await userEvent.type(screen.getByPlaceholderText('add todo'), 'test todo');
+    await userEvent.click(screen.getByRole('button'))
+    expect(screen.getByPlaceholderText('add todo')).toHaveValue("")
+  });
+})
+
