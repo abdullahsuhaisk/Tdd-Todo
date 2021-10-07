7b90ca0a9e2617d2d27fc6990c8ddfc2165c81f6
Abdullah Süha ışık <abdullahsuhaisik@gmail.com>
2021-10-08 01:01:49 +0300

    The first part is ok for front-end

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,12 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  // console.log(todo)
+  function handleClick() {
+    setTodo('')
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        <div className="Todo-container">
+          <h3>TODOS</h3>
+          <div className="Todo-input">
+            <input placeholder="add todo" type="text" id="todo-add" onChange={(e) => { setTodo(e.target.value) }} value={todo} />
+            <button onClick={() => handleClick()}>Add todo</button>
+          </div>
+          <ul className="Todo-list" id="Todo">
+            {todoList.length > 0 ? todoList.map(todoItem => {
+              return (<li>
+                {todoItem}
+              </li>)
+            }) : null}
+          </ul>
+        </div>
       </header>
     </div>
   );
