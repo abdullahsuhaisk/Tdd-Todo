9141fdbcdc56626ddeb267e8fd214b97113ac896
Abdullah Süha ışık <abdullahsuhaisik@gmail.com>
2021-10-08 14:58:12 +0300

    added axios and backend connection

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
+import axios from 'axios';
 
 function App() {
   const [todo, setTodo] = useState('');
   const [todoList, setTodoList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function addTodoBackend(todoParam) {
+    setLoading(true)
+    try {
+      const response = await axios.post('http://localhost:3001/todo', { todo: todoParam })
+      const { data: {id, message} } = response
+      alert(`${message} your todo is : ${todo}`)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+    }
+  }
 
   // console.log(todo)
   function handleClick() {
+    addTodoBackend(todo)
     setTodoList([...todoList,todo])
     setTodo('')
   }
@@ -20,7 +34,7 @@ function App() {
           <h3>TODOS</h3>
           <div className="Todo-input">
             <input placeholder="add todo" type="text" id="todo-add" onChange={(e) => { setTodo(e.target.value) }} value={todo} />
-            <button onClick={() => handleClick()} id="addButton">Add todo</button>
+            {!loading ? <button onClick={() => handleClick()} id="addButton">Add todo</button>: null}
           </div>
           <ul className="Todo-list" id="Todo">
             {todoList.length > 0 ? todoList.map(todoItem => {
